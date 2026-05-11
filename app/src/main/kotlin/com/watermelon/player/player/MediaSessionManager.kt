c528a27c673ee88554fe015a2ea3351afab6317c
So Muzaffari <so.muzaff@gmail.com>
2026-05-12 03:04:46 +0330

    Update MediaSessionManager.kt

--- app/src/main/kotlin/com/watermelon/player/player/MediaSessionManager.kt
+++ app/src/main/kotlin/com/watermelon/player/player/MediaSessionManager.kt
@@ -3,7 +3,6 @@ package com.watermelon.player.player
 import android.content.Context
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import androidx.media.session.MediaButtonReceiver
 
 class MediaSessionManager(
     private val context: Context,
@@ -11,16 +10,15 @@ class MediaSessionManager(
 ) {
     private val mediaSession = MediaSessionCompat(context, "WatermelonPlayer").apply {
         setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
-        setPlaybackState(PlaybackStateCompat.Builder()
-            .setState(PlaybackStateCompat.STATE_NONE, 0, 1f)
-            .setActions(
-                PlaybackStateCompat.ACTION_PLAY or
-                PlaybackStateCompat.ACTION_PAUSE or
-                PlaybackStateCompat.ACTION_SEEK_TO or
-                PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
-                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
-            )
-            .build()
+        setPlaybackState(
+            PlaybackStateCompat.Builder()
+                .setState(PlaybackStateCompat.STATE_NONE, 0, 1f)
+                .setActions(
+                    PlaybackStateCompat.ACTION_PLAY or
+                    PlaybackStateCompat.ACTION_PAUSE or
+                    PlaybackStateCompat.ACTION_SEEK_TO
+                )
+                .build()
         )
         isActive = true
     }
