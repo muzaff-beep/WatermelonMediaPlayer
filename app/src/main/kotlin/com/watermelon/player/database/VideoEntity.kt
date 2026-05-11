b831c9ac45c6aaa3769c1fecaf7856dadd08401c
So Muzaffari <so.muzaff@gmail.com>
2026-05-12 02:33:49 +0330

    Update VideoEntity.kt

--- app/src/main/kotlin/com/watermelon/player/database/VideoEntity.kt
+++ app/src/main/kotlin/com/watermelon/player/database/VideoEntity.kt
@@ -1,16 +1,15 @@
 package com.watermelon.player.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "videos")
 data class VideoEntity(
-    @PrimaryKey val id: String,
-    val title: String,
-    val path: String,
-    val duration: Long
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "uri") val uri: String,          // content:// or file://
+    @ColumnInfo(name = "duration") val duration: Long,
+    @ColumnInfo(name = "folder_path") val folderPath: String,
+    @ColumnInfo(name = "hash") val hash: String = ""    // MD5 future use
 )
-
-// Already has id, title, path, duration – add:
-@ColumnInfo(name = "folder_path")
-val folderPath: String
