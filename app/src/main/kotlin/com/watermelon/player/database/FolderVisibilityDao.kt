94c535a6cfbba6c3253773c6391b670d9405d255
So Muzaffari <so.muzaff@gmail.com>
2026-05-12 02:35:02 +0330

    Update FolderVisibilityDao.kt

--- app/src/main/kotlin/com/watermelon/player/database/FolderVisibilityDao.kt
+++ app/src/main/kotlin/com/watermelon/player/database/FolderVisibilityDao.kt
@@ -1,12 +1,22 @@
-// database/FolderVisibilityDao.kt
+package com.watermelon.player.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface FolderVisibilityDao {
     @Query("SELECT * FROM folder_visibility WHERE isVisible = 0")
-    fun getExcludedFolders(): Flow<List<FolderVisibility>>
+    fun getHiddenFolders(): Flow<List<FolderVisibility>>
 
     @Query("SELECT isVisible FROM folder_visibility WHERE folderPath = :path")
     suspend fun isFolderVisible(path: String): Boolean?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun setVisibility(folder: FolderVisibility)
+
+    @Query("DELETE FROM folder_visibility WHERE folderPath = :path")
+    suspend fun deleteVisibility(path: String)
 }
