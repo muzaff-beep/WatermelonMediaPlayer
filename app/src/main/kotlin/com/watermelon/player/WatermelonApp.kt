92252a3e1d493a38bcf7842929e5abe26b7c904d
So Muzaffari <so.muzaff@gmail.com>
2026-05-12 03:01:32 +0330

    Update WatermelonApp.kt

--- app/src/main/kotlin/com/watermelon/player/WatermelonApp.kt
+++ app/src/main/kotlin/com/watermelon/player/WatermelonApp.kt
@@ -1,14 +1,11 @@
 package com.watermelon.player
 
 import android.app.Application
-import com.watermelon.player.database.MediaDatabase
+import com.watermelon.player.scan.ScanScheduler
 
 class WatermelonApp : Application() {
-    // DB instance can be accessed via MediaDatabase.getDatabase(this)
-    // Koin initialization can go here later
-
     override fun onCreate() {
         super.onCreate()
-        // Initialize anything global
+        ScanScheduler.schedule(this)
     }
 }
