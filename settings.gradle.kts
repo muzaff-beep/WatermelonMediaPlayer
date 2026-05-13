fbff2314109c8f0b7eb6d138f8a22168bb3bd1e1
So Muzaffari <so.muzaff@gmail.com>
2026-05-13 21:30:18 +0330

    Update settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WatermelonMediaPlayer"
+include(":app")
