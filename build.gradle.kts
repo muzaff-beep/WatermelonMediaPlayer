4e9e06ed6726ab910d5d6916f8c2f60454c7bb11
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 05:40:38 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
+// build.gradle.kts (root)
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.8" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
