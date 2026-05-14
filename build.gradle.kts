8ce242210101db4e06b23dfc7c4ad8341aaec866
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 12:22:34 +0330

    Update build.gradle.kts
    Signed-off-by: So Muzaffari  <so.muzaff@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-// build.gradle.kts (root)
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
