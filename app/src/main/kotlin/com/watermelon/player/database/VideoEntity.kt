c893f4ebea1874d05b06e3e4ef05aa46be038677
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 12:09:34 +0330

    Update VideoEntity.kt

--- app/src/main/kotlin/com/watermelon/player/database/VideoEntity.kt
+++ app/src/main/kotlin/com/watermelon/player/database/VideoEntity.kt
@@ -1,12 +1,8 @@
 // app/src/main/kotlin/com/watermelon/player/database/VideoEntity.kt
 package com.watermelon.player.database
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "videos")
 data class VideoEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val id: Long = 0,
     val uri: String,
     val displayName: String,
     val mimeType: String,
