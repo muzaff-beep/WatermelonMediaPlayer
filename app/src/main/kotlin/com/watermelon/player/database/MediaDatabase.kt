0ec88cb7fb1ba1cddaf5fde031c628db720dd169
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 12:11:00 +0330

    Update MediaDatabase.kt

--- app/src/main/kotlin/com/watermelon/player/database/MediaDatabase.kt
+++ app/src/main/kotlin/com/watermelon/player/database/MediaDatabase.kt
@@ -2,34 +2,8 @@
 package com.watermelon.player.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
 
-@Database(
-    entities = [VideoEntity::class, FolderVisibility::class],
-    version = 1,
-    exportSchema = false
-)
-abstract class MediaDatabase : RoomDatabase() {
-    abstract fun videoDao(): VideoDao
-    abstract fun folderVisibilityDao(): FolderVisibilityDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: MediaDatabase? = null
-
-        fun getInstance(context: Context): MediaDatabase {
-            return INSTANCE ?: synchronized(this) {
-                INSTANCE ?: Room.databaseBuilder(
-                    context.applicationContext,
-                    MediaDatabase::class.java,
-                    "watermelon_media.db"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                    .also { INSTANCE = it }
-            }
-        }
-    }
+class MediaDatabase(context: Context) {
+    val videoDao = VideoDao(context)
+    val folderVisibilityDao = FolderVisibilityDao(context)
 }
