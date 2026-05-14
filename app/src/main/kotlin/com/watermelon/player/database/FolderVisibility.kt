bbcb967da0b0dbf7a5af9da88aed4e8e671c0518
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 12:10:19 +0330

    Update FolderVisibility.kt

--- app/src/main/kotlin/com/watermelon/player/database/FolderVisibility.kt
+++ app/src/main/kotlin/com/watermelon/player/database/FolderVisibility.kt
@@ -1,11 +1,7 @@
 // app/src/main/kotlin/com/watermelon/player/database/FolderVisibility.kt
 package com.watermelon.player.database
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "folder_visibility")
 data class FolderVisibility(
-    @PrimaryKey val folderUri: String,
+    val folderUri: String,
     val isVisible: Boolean = true
 )
